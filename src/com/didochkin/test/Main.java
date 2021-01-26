540a3d15034f31188167ee8580f6ac09af871329
Alexander Dydochkin <a.dydochkin@syberry.com>
2021-01-26 11:30:51 +0300

    commit3.1

--- src/com/didochkin/test/Main.java
+++ src/com/didochkin/test/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ktgctgc.;ujtcljutgc
+                ljcljugc ;ic/ik
+                ljugc ljhc mjcjc jcjvc
+                v/ik'v/'/gbkughonhloih
     }
 }
